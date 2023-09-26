d13c1a30622a2ede6bfbe5509d15235d4a82b59b
Egres Kata <egres.kata0@gmail.com>
2023-09-26 22:02:14 +0200

    added summary code for delegates

---
+++ lab01/lambda/lambda/lambda/Feedback.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambda
+{
+    enum Category { Opinion, BugReport, FeatureRequest }
+    internal class Feedback
+    {
+        public Category Category { get; set; }
+        public int Priority { get; set; }
+        public string Product { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Category} of priority {Priority}: {Product} - {Description}";
+        }
+
+    }
+
+    internal class FeedbackProcessor
+    {
+        const int LIMIT = 3;
+        Dictionary<Category, Action<Feedback>> feedbackActions;
+        List<Feedback> feedbacks;
+
+        public FeedbackProcessor(Action<Feedback> defaultAction)
+        {
+            feedbacks = new List<Feedback>();
+            feedbackActions = new Dictionary<Category, Action<Feedback>>();
+
+            foreach (Category item in Enum.GetValues(typeof(Category)))
+            {
+                feedbackActions.Add(item, defaultAction);
+            }
+        }
+
+        public void AddAction(Category category, Action<Feedback> method)
+        {
+            feedbackActions[category] += method;
+        }
+
+        public void AddFeedback(Feedback feedback)
+        {
+            feedbacks.Add(feedback);
+            if (feedbacks.Count == LIMIT)
+            {
+                foreach (Feedback item in feedbacks)
+                {
+                    feedbackActions[item.Category].Invoke(item);
+                    Console.WriteLine("FEEDBACK PROCESSED");
+                    Console.WriteLine();
+                }
+                feedbacks.Clear();
+            }
+        }
+    }
+}
---
+++ lab01/lambda/lambda/lambda/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace lambda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // default
+            FeedbackProcessor processor = new FeedbackProcessor((feedback) =>
+            {
+                Console.WriteLine($"Default action for the feedback of {feedback.Category}");
+            });
+
+            Feedback f1 = new Feedback() { Category = Category.Opinion, Priority = 1, Product = "Edge", Description = "Yuck" };
+            Feedback f2 = new Feedback() { Category = Category.BugReport, Priority = 6, Product = "Chrome", Description = "Froze 13 times" };
+            Feedback f3 = new Feedback() { Category = Category.FeatureRequest, Priority = 9, Product = "Firefox", Description = "Make it better pls" };
+
+            processor.AddFeedback(f1);
+            processor.AddFeedback(f2);
+            processor.AddFeedback(f3);
+
+            Console.ReadLine();
+
+            // #1
+            Action<Feedback> writeOut = (fb) =>
+            {
+                Console.WriteLine(fb.ToString());
+            };
+
+            // #2
+            Action<Feedback> saveToFile = (fb) =>
+            {
+                File.AppendAllText("feedbacks.log", fb.ToString() + "\r\n");
+                Console.WriteLine("FILE UPDATED");
+            };
+
+            processor.AddAction(Category.Opinion, WriteToConsole);
+            processor.AddAction(Category.BugReport, writeOut);
+            processor.AddAction(Category.FeatureRequest, writeOut);
+
+            processor.AddAction(Category.BugReport, saveToFile);
+            processor.AddAction(Category.FeatureRequest, saveToFile);
+
+            processor.AddFeedback(f1);
+            processor.AddFeedback(f2);
+            processor.AddFeedback(f3);
+
+            Console.ReadLine();
+        }
+
+        // #1 other
+        static void WriteToConsole(Feedback fb)
+        {
+            Console.WriteLine(fb.ToString());
+        }
+    }
+}
