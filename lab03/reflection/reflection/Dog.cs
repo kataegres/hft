04ab2ef06be992e4edace95c2f525b3d0a7dc237
Kata Egres <egres.kata0@gmail.com>
2023-10-04 16:03:39 +0200

    added summary code for reflection/attributes

---
+++ lab03/reflection/reflection/Dog.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reflection
+{
+    [Obsolete]
+    internal class Dog
+    {
+        public bool adopted = false;
+        public Dog(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public int Age { get; set; }
+        
+        [Description("Születési név")]
+        public string Name { get; set; }
+
+        [Description("Kutyaugatás")]
+        public void Bark()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    class Spaniel : Dog
+    {
+        public Spaniel(int age, string name) : base(age, name)
+        {
+        }
+    }
+
+    class ValamiAttribute : Attribute
+    {
+        //...
+    }
+}
---
+++ lab03/reflection/reflection/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Típusinformáció osztályról
+            Type t = typeof(int);
+            Type t2 = typeof(string);
+            Type t3 = typeof(List<int>);
+            Type t4 = typeof(Dog);
+            ;
+
+            // Objektumról
+            Dog d = new Dog(2, "Morzsi");
+            Spaniel s = new Spaniel(2, "Morzsi");
+            GetInfo(s);
+
+            // Tulajdonság
+            PropertyInfo info = typeof(Dog).GetProperty("Age");
+            ;
+            Console.WriteLine(info);
+        }
+        static void GetInfo(object obj)
+        {
+            Type t = obj.GetType();
+            Console.WriteLine(t.Name);
+            Console.WriteLine(t.FullName);
+            Console.WriteLine(t.BaseType);
+        }
+    }
+}
